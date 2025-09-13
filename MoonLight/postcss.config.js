182077a60e079b272394fa7f463316073aa25e7a
Hyowon An <gydnjs3505@gmail.com>
2025-09-13 13:36:21 +0900

    에러 해결로 정상동작함. css 제대로 입혀진거 확인. "npm i -D tailwindcss postcss autoprefixer @tailwindcss/postcss " @tailwindcss/postcss 패키지 설치 posstcss.config.js 파일 esm방식으로 수정(이러니까 오류 안 났음)

--- MoonLight/postcss.config.js
+++ MoonLight/postcss.config.js
@@ -1,7 +1,24 @@
-// postcss.config.js
+// ❌ 구방식
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+
+// tailwind 4 방식
+// postcss.config.js (CJS)
+// module.exports = {
+//   plugins: {
+//     "@tailwindcss/postcss": {},  // ✅ 이걸로 변경
+//     autoprefixer: {},
+//   },
+// }
+
+// postcss.config.js (ESM)
 export default {
   plugins: {
-    tailwindcss: {},
+    "@tailwindcss/postcss": {},  // ✅
     autoprefixer: {},
   },
-};
+}
